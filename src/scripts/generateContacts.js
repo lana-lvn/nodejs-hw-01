7447ce9b6a9dc17ca75753f588073c6ae2f05229
ruslana <ruslana.lohvyn@gmail.com>
2025-02-22 13:29:52 +0200

    hw-01-done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,18 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
-generateContacts(5);
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+
+    for (let i = 0; i < number; i++) {
+      contacts.push(createFakeContact());
+    }
+    await writeContacts(contacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+generateContacts(2);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,17 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    if (contacts.length !== 0) {
+      contacts.pop();
+      return await writeContacts(contacts);
+    }
+    return;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeLastContact();
